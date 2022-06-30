40d2e7ce379cabfae7a221fead897aca1de19a1f
lylapitajen <pitajenlyla@gmail.com>
2022-06-30 23:28:53 +0100

    first four chapter on freecodecamp.org

---
+++ Chapter 4 Functions.py
@@ -0,0 +1,13 @@
+def greet(lang):
+    if lang == "English":
+        print("Hello!")
+    elif lang == "Spanish":
+        print("Hola!")
+    elif lang == "French":
+        print("Bonjour!")
+
+lang = input("Enter language:")
+greet(lang)
+git config --global user.email "pitajenlyla@gmail.com"
+git config --global user.name "lylapitajen"
+
---
+++ Chapter2 Expressions.py
@@ -0,0 +1,18 @@
+# e = "hello  " + "world"
+# print(e)
+ 
+
+# celsius = input("Celsius:")
+# far = (float(celsius) * 1.8) + 32
+# print("Fahrenheit:", far )
+
+#Ex 1
+# name = input("Enter your name: ")
+# print("Hello", name)
+
+#Ex 2
+hours = input("Enter Hours: ")
+rate = input("Enter Rate: £")
+pay = float(hours) * float(rate)
+pay = round(pay,2)
+print("Pay: £", pay)
