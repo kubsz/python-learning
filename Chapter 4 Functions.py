b1c5d7cec804af128a51af644d476e2897fd46e8
lylapitajen <pitajenlyla@gmail.com>
2022-07-02 12:52:45 +0100

    added exercises in chapter 4 and 5

--- Chapter 4 Functions.py
+++ Chapter 4 Functions.py
@@ -1,12 +1,42 @@
-def greet(lang):
-    if lang == "English":
-        print("Hello!")
-    elif lang == "Spanish":
-        print("Hola!")
-    elif lang == "French":
-        print("Bonjour!")
+# def greet(lang):
+#     if lang == "English":
+#         print("Hello!")
+#     elif lang == "Spanish":
+#         print("Hola!")
+#     elif lang == "French":
+#         print("Bonjour!")
 
-lang = input("Enter language:")
-greet(lang)
+# lang = input("Enter language:")
+# greet(lang)
 
+#Ex. 6 - Rewrite your pay computation with time and a half for overtime and 
+# create a function called computepay which takes two parameters (hours and pay)
 
+# def computepay (hours,rate):
+#     hours = float(hours)
+#     rate = float(rate)
+#     othours = hours - 40
+#     otrate = 1.5 * rate
+#     if hours <= 40:
+#         pay = hours * rate
+#         pay = round(pay,2)
+#         print ("Pay:£ ",pay)
+#     else:
+#         pay = (40 * rate) + (othours * otrate)
+#         pay = round(pay, 2)
+#         print("Pay:£ ", pay)
+
+# computepay(56,10)
+
+#Ex. 7 - Write a function called computegrade that takes a score as its parameter 
+# and returns a grade as a string
+
+def computegrade(score):
+    if score >= 0.9:
+        print ("Grade:A")
+    elif score == 0.8:
+        print ("Grade:B")
+    elif score == 0.7:
+        print ("Grade:C")
+
+computegrade(0.7)
---
+++ Chapter 5 Loops and Iterations.py
@@ -0,0 +1,28 @@
+#Ex. Write a program which repeatedly reads numbers until the user types "done". Once "done" is entered, print out the total, count
+#and average of the numbers, If the user enters anything other than a number, detect their mistake using try and ecvept and print out 
+#an error message and skip to the next number
+
+count = 0
+total = 0.0
+while True:
+    user_input = input("Enter a number:")
+    if user_input == "done":
+        if count == 0:
+            continue
+        break
+    try:
+        user_input = float(user_input)
+    except:
+        print("Invalid input")
+        continue
+    count = count +  1
+    total = total + user_input
+
+print (total, count, (total/count))
+    
+
+
+
+
+
+
