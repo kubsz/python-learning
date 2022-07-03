1be7a4a7a7cf280394416d35485961248c45c63a
lylapitajen <pitajenlyla@gmail.com>
2022-07-03 12:33:49 +0100

    added chapter 6 and 7 + extra exercises

---
+++ Chapter 6 Strings.py
@@ -0,0 +1,9 @@
+#looping through strings
+# sugar = "glucose"
+# for letter in sugar:
+#     print(letter)
+
+#Generating usernames using first three characters of first name and surname
+first_name = input ("Enter first name:")
+surname = input("Enter surname:")
+username = print("Username:", first_name[0:3] + surname[0:3])
---
+++ Chapter 7 Reading Files.py
@@ -0,0 +1,29 @@
+#Ex.1 - Write a program to read through a file and print the contents of the file
+#line by line in upper case
+
+# file_name = input("Enter a file name:")
+# file = open(file_name)
+# file_content = file.read()
+# print(file_content.upper())
+
+#Ex. 2 - User enters file name, open file, count how many characters in each line and print the line with the most characters
+
+def Find_line_w_most_characters(file):
+    highest_line_content = ' '
+    for line in file:
+        line = line.rstrip()
+        if len(line) > len(highest_line_content):
+            highest_line_content = line
+    print(highest_line_content, len(highest_line_content))
+
+while True:
+    file_name = input("Enter file name:")
+    try:
+        file = open(file_name)
+        break
+    except:
+        print("File cannot be found")
+        continue
+Find_line_w_most_characters(file)
+
+
